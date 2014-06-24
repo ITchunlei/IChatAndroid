28145c26af4a73c15b2b33bc3d1f2ac4a2984821
wangchunlei <ITchunlei@163.com>
2014-06-24 13:00:24 +0800

    add ichat activity

--- src/com/chunlei/ichat/IChatActivity.java
+++ src/com/chunlei/ichat/IChatActivity.java
@@ -10,7 +10,7 @@ public class IChatActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.ichat);
 	}
 	
 	@Override
