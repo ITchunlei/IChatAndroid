2757c6c0f9563bd5d30a77c017d788922bbdd350
wangchunlei <ITchunlei@163.com>
2014-06-24 16:59:50 +0800

    normal commit

--- src/com/chunlei/ichat/IChatActivity.java
+++ src/com/chunlei/ichat/IChatActivity.java
@@ -1,24 +1,45 @@
 package com.chunlei.ichat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnDragListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class IChatActivity extends Activity {
 	private ViewPager mViewPager;
+	private IChatViewPagerAdapter mAdapter;
+	private List<View> mViews;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ichat);
 		mViewPager = (ViewPager) findViewById(R.id.view_pager);
-		mViewPager.addView(LayoutInflater.from(this).inflate(R.layout.chat, null));
 		mViewPager.setCurrentItem(0);
+		
+		mViews = new ArrayList<View>();
+		mViews.add(LayoutInflater.from(this).inflate(R.layout.chat, null));
+		mViews.add(LayoutInflater.from(this).inflate(R.layout.discovery, null));
+		mViews.add(LayoutInflater.from(this).inflate(R.layout.contacts, null));
+		
+		getFragmentManager().beginTransaction().commit();
+		mAdapter = new IChatViewPagerAdapter(mViews);
+		mViewPager.setAdapter(mAdapter);
 	}
 	
 	@Override
@@ -41,19 +62,27 @@ public class IChatActivity extends Activity {
 	
 	
 	class IChatViewPagerAdapter extends PagerAdapter {
-
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return 0;
+		private List<View> mViews;
+		public IChatViewPagerAdapter(List<View> views) {
+			mViews = views;
 		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
+		public int getCount() {  
+	        return mViews.size();
+	    }  
+	    @Override  
+	    public void destroyItem(ViewGroup container, int position, Object object) { 
+	
+	    }  
+	    @Override  
+	    public Object instantiateItem(ViewGroup container, int position) {
+	    	View view = mViews.get(position);
+	    	container.addView(view);
+	        return view;  
+	    }  
+	    @Override  
+	    public boolean isViewFromObject(View view, Object object) {  
+	        return view == object;  
+	    }  
 		
 	}
 
